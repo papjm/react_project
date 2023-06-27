b6c310a2c802d63c3923e479691b3ef2423e9bfa
papjm <102404994+papjm@users.noreply.github.com>
2023-06-27 13:11:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-var React = require("react");
-var ReactDOM = require("react-dom")
+import React from "react";
+import ReactDOM from "react-dom";
+// var React = require("react");
+// var ReactDOM = require("react-dom")
 
+ReactDOM.render(<h1>Da Vinci !</h1>, document.getElementById("root"));
 
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
+    <h1>Da Vinci!</h1>
+    <ul>
+      <li>Hack</li>
+      <li>Er</li>
+      <li>One</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
